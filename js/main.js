81c0f816792802ac19c33793a80c74aca77ea71e
artemisia000 <misiapierobon@gmail.com>
2021-10-25 17:31:49 +0200

    if

--- js/main.js
+++ js/main.js
@@ -5,6 +5,24 @@ const boxes = document.querySelector('.boxes');
 //genera boxes
 
 for (let i = 1; i <= 100; i++){
-    boxes.innerHTML += `<div class="box box-${i}">${i}</div>`;
+    boxes.innerHTML += `<div class="box box-${i}">${i}></div>`;
+
 }
+
+
+const unit = 1
+
+let fizz = unit % 3;
+console.log(`moltiplicazione:`, fizz);
+
+if() {
+
+   
+
+}   
+
+
+
+
+
 
