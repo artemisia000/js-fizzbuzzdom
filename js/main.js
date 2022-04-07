a3f9238de6fd86b1271b737e0f3b52df3bd44815
artemisia000 <misiapierobon@gmail.com>
2022-04-07 17:20:08 +0200

    ok

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 const boxes = document.querySelector('.boxes');
 
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
  
     if (i % 3 == 0 && i % 5 == 0) {
                 square = `<div class="box box-fizzbuzz">fizzbuz</div>`;
@@ -22,44 +22,10 @@ for (let i = 0; i <= 100; i++) {
             }
 
             boxes.innerHTML += square;
-            console.log(square);
+
 }
 
 
-
-
-
-
-
-
-
-
-
-
-// //print dei box
-
-// for (let i = 0; i <= 100; i++){
-    
-
-//     if (i % 3 == 0 && i % 5 == 0) {
-//         square = `<div class="box box-fizzbuzz">fizzbuz</div>`;
-//     }
-
-//     else if (i % 3 == 0) {
-//         square = `<div class="box box-fizz">fizz</div>`;
-//     }
-
-//     else if (i % 5 == 0) {
-//         square = `<div class="box box-buzz">buzz</div>`;
-//     }
-
-//     else {
-//         square = `<div class="box "> ${i} </div>`;
-//     }
-
-//     boxes.innerHTML += element;
-
-// }
 
 
 
